9060ea3806913fe314001d489b4344762f462352
Isabella <isabellaoliveira.trab@gmail.com>
2022-11-24 10:22:06 -0300

    node.js aula01

---
+++ aula1.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function(req, res){
+    res.write('olha se vc nao me ama \n');
+    res.end('Olá meus amigos de salvador aqui qm fala eh o manuel gome o caneta azulll');
+}).listen(3000);
